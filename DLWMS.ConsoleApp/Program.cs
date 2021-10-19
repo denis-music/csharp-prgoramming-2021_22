79b9211950e2bfbc655a0b2f74d513c579bf06cb
Denis Mušić <denis@edu.fit.ba>
2021-10-19 10:05:24 +0200

    Inicijalni commit

---
+++ DLWMS.ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using DLWMS.ConsoleApp.Helper;
+using DLWMS.ConsoleApp.Predavanja;
+using DLWMS.ConsoleApp.Predavanja.P2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//include
+
+namespace DLWMS.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MainP2.Pokreni();
+        }
+    }
+}
