1d0c4c6d1582db92ff97afb727346c8fef1bbcbb
Denis Mušić <denis@edu.fit.ba>
2021-10-26 14:52:49 +0200

    P4 :: Promjene

---
+++ DLWMS.WinForms/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DLWMS.WinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "Nastava iz Programiranja III";
+        }
+
+       
+    }
+
+    public partial class Student
+    {
+        public string ImePrezime { get; set; }
+    }
+}
